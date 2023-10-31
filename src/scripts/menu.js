bc3abe7d463835b78430f1f52fd46ac3226c8e61
phibkro <philib.krogh@gmail.com>
2023-10-31 17:05:29 +0100

    feat: :sparkles: add images

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -15,28 +15,41 @@ const data = {
 			category: "Pizza",
 			price: 3.99,
 			ingredients: ["tomato", "cheese", "pepperoni"],
-			description: "A classic pizza with tomato, cheese and pepperoni"
+			description: "A classic pizza with tomato, cheese and pepperoni",
+			src: "./assets/images/pizza/pepperoni_victor-miyata.jpg",
+			alt: "Pizza Pepperoni",
+			credit: "Photo by Victor Miyata from Pexels"
 		},
 		{
 			name: "Carbonara",
 			category: "Pasta",
 			price: 4.99,
 			ingredients: ["pasta", "bacon", "egg"],
-			description: "A classic pasta with bacon and egg"
+			description: "A classic pasta with bacon and egg",
+			src: "./assets/images/pasta/carbonara_alejandro-aznar.jpg",
+			alt: "Carbonara",
+			credit: "Photo by Alejandro Aznar on Pexels"
 		},
 		{
 			name: "Bolognese",
 			category: "Pasta",
 			price: 4.99,
 			ingredients: ["pasta", "minced meat", "tomato"],
-			description: "A classic pasta with minced meat and tomato"
+			description: "A classic pasta with minced meat and tomato",
+			src: "./assets/images/pasta/bolognese_monica-turlui.jpg",
+			alt: "Bolognese",
+			credit: "Photo by Monica Turlui on Pexels"
 		},
 		{
 			name: "Lasagne",
 			category: "Pasta",
 			price: 5.99,
 			ingredients: ["pasta", "minced meat", "tomato", "cheese"],
-			description: "A classic lasagne with minced meat, tomato and cheese"
+			description:
+				"A classic lasagne with minced meat, tomato and cheese",
+			src: "./assets/images/pasta/lasagne_alleksana.jpg",
+			alt: "Lasagne",
+			credit: "Photo by alleksana from Pexels"
 		},
 		{
 			name: "Tiramisu",
@@ -44,24 +57,36 @@ const data = {
 			price: 2.99,
 			ingredients: ["mascarpone", "coffee", "biscuits"],
 			description:
-				"A classic tiramisu with mascarpone, coffee and biscuits"
+				"A classic tiramisu with mascarpone, coffee and biscuits",
+			src: "./assets/images/dessert/tiramisu_taqnia-creative-studio.jpg",
+			alt: "Tiramisu",
+			credit: "Photo by Taqnia Creative Studio from Pexels"
 		},
 		{
 			name: "Panna Cotta",
 			category: "Dessert",
 			price: 2.99,
 			ingredients: ["cream", "vanilla", "sugar"],
-			description: "A classic panna cotta with cream, vanilla and sugar"
+			description: "A classic panna cotta with cream, vanilla and sugar",
+			src: "./assets/images/dessert/panna-cotta_pixabay.jpg",
+			alt: "Panna Cotta",
+			credit: "Photo by Pixabay from Pexels"
 		},
 		{
-			name: "Coca Cola",
+			name: "Coca-Cola",
 			category: "Drinks",
-			price: 1.99
+			price: 1.99,
+			src: "./assets/images/drink/coca-cola_pixabay.jpg",
+			alt: "Coca-Cola",
+			credit: "Photo by Pixabay from Pexels"
 		},
 		{
 			name: "Fanta",
 			category: "Drinks",
-			price: 1.99
+			price: 1.99,
+			src: "./assets/images/drink/fanta_sandu-muresan.jpg",
+			alt: "Fanta",
+			credit: "Photo by Sandu Muresan from Pexels"
 		}
 	]
 };
