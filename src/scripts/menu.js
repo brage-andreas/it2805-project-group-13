26aa1febb0c673ab7a82010093d9945eda5cd51c
phibkro <philib.krogh@gmail.com>
2023-10-31 15:54:44 +0100

    feat: :clown_face: declare menu items data

---
+++ src/scripts/menu.js
@@ -0,0 +1,67 @@
+const data = {
+	items: [
+		{
+			name: "Margherita",
+			category: "Pizza",
+			price: 2.99,
+			ingredients: ["tomato", "cheese"],
+			description: "A classic pizza with tomato and cheese",
+			src: "./assets/images/pizza/margherita_rene-strgar.jpg",
+			alt: "Pizza Margherita",
+			credit: "Photo by Rene Strägar on Pexels"
+		},
+		{
+			name: "Pepperoni",
+			category: "Pizza",
+			price: 3.99,
+			ingredients: ["tomato", "cheese", "pepperoni"],
+			description: "A classic pizza with tomato, cheese and pepperoni"
+		},
+		{
+			name: "Carbonara",
+			category: "Pasta",
+			price: 4.99,
+			ingredients: ["pasta", "bacon", "egg"],
+			description: "A classic pasta with bacon and egg"
+		},
+		{
+			name: "Bolognese",
+			category: "Pasta",
+			price: 4.99,
+			ingredients: ["pasta", "minced meat", "tomato"],
+			description: "A classic pasta with minced meat and tomato"
+		},
+		{
+			name: "Lasagne",
+			category: "Pasta",
+			price: 5.99,
+			ingredients: ["pasta", "minced meat", "tomato", "cheese"],
+			description: "A classic lasagne with minced meat, tomato and cheese"
+		},
+		{
+			name: "Tiramisu",
+			category: "Dessert",
+			price: 2.99,
+			ingredients: ["mascarpone", "coffee", "biscuits"],
+			description:
+				"A classic tiramisu with mascarpone, coffee and biscuits"
+		},
+		{
+			name: "Panna Cotta",
+			category: "Dessert",
+			price: 2.99,
+			ingredients: ["cream", "vanilla", "sugar"],
+			description: "A classic panna cotta with cream, vanilla and sugar"
+		},
+		{
+			name: "Coca Cola",
+			category: "Drinks",
+			price: 1.99
+		},
+		{
+			name: "Fanta",
+			category: "Drinks",
+			price: 1.99
+		}
+	]
+};
